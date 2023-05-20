f2c8a225946ab7d56dde9dec3f017bd949a4d52b
PandaNinjas <admin@malwarefight.wip.la>
2023-05-20 11:57:08 -0400

    More caching

--- main.sh
+++ main.sh
@@ -2,5 +2,4 @@
 
 docker build -t pimachinelearning/pitorch-builder .
 docker volume create ccache
-echo "CCACHE_MOUNT=$(docker volume inspect ccache | jq '.[0]["Mountpoint"]')" >> $GITHUB_OUTPUT
 docker run -e IGNORE_CORES=0 -e CCACHE_DIR=/ccache --mount source=ccache,target=/ccache pimachinelearning/pitorch-builder
