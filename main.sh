ee08fa45b972abda8842943bcedffba5c485be31
PandaNinjas <admin@malwarefight.wip.la>
2023-05-20 14:44:34 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,4 +2,5 @@
 
 docker build -t pimachinelearning/pitorch-builder .
 docker volume create ccache
-docker run -e IGNORE_CORES=0 -e CCACHE_DIR=/ccache --mount source=ccache,target=/ccache pimachinelearning/pitorch-builder
+        # 5 hours 45 minutes, allow 15 minutes for the other the tasks
+timeout 20700 docker run -e IGNORE_CORES=0 -e CCACHE_DIR=/ccache --mount source=ccache,target=/ccache pimachinelearning/pitorch-builder
