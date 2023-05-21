0b646ad790875e5137dc533ba180d8c72836d442
PandaNinjas <admin@malwarefight.wip.la>
2023-05-21 10:28:57 -0400

    5 hour timeout

--- main.sh
+++ main.sh
@@ -2,5 +2,5 @@
 
 docker build -t pimachinelearning/pitorch-builder .
 docker volume create ccache
-# kill -9 to make sure it's killed    5 hours 45 minutes, allow 15 minutes for the other the tasks
-timeout --signal=9 20700 docker run -e IGNORE_CORES=0 -e CCACHE_DIR=/ccache --mount source=ccache,target=/ccache pimachinelearning/pitorch-builder
+# kill -9 to make sure it's killed    5 hours, allow 1 hour for the other the tasks
+timeout --signal=9 18000 docker run -e IGNORE_CORES=0 -e CCACHE_DIR=/ccache --mount source=ccache,target=/ccache pimachinelearning/pitorch-builder
