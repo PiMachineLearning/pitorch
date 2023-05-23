74f5084260e5a39ea694ab92826fd1cd94ef147c
PandaNinjas <admin@malwarefight.wip.la>
2023-05-23 11:25:57 -0400

    [ci skip] chmod -R in first location

--- main.sh
+++ main.sh
@@ -4,4 +4,4 @@ docker build -t pimachinelearning/pitorch-builder .
 docker volume create ccache
 # kill -9 to make sure it's killed    5 hours, allow 1 hour for the other the tasks
 timeout --signal=9 18000 docker run -e IGNORE_CORES=0 -e CCACHE_DIR=/ccache --mount source=ccache,target=/ccache pimachinelearning/pitorch-builder
-sudo chmod 777 /var/lib/docker/volumes/ccache/_data
+sudo chmod -R 777 /var/lib/docker/volumes/ccache/_data
