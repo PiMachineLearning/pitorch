3f15d4cf9238e3567d07b1250745ac968d8ef674
PandaNinjas <admin@malwarefight.wip.la>
2023-05-24 20:17:39 -0400

    Debug volume

--- main.sh
+++ main.sh
@@ -5,3 +5,4 @@ docker volume create ccache
 # kill -9 to make sure it's killed    5 hours, allow 1 hour for the other the tasks
 timeout --signal=9 18000 docker run -e IGNORE_CORES=0 -e CCACHE_DIR=/ccache --mount source=ccache,target=/ccache pimachinelearning/pitorch-builder
 sudo chmod -R 777 /var/lib/docker/volumes/ccache/_data
+find /var/lib/docker/volumes/ccache/_data
