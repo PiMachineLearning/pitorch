19b495d3194f41509da184d811cf0a26c3e1d08b
PandaNinjas <admin@malwarefight.wip.la>
2023-05-26 12:45:38 -0400

    Only build if we have to

--- main.sh
+++ main.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-
-docker build -t pimachinelearning/pitorch-builder .
+docker pull pimachinelearning/pitorch-builder
+if [ $? -eq 1 ]; then
+  docker build -t pimachinelearning/pitorch-builder .
+  docker tag pimachinelearning/pitorch-builder:latest pimachinelearning/raspi-python:latest
+  docker push pimachinelearning/raspi-python:latest
+fi
 docker volume create ccache
 # kill -9 to make sure it's killed    5 hours, allow 1 hour for the other the tasks
 timeout --signal=9 18000 docker run -e IGNORE_CORES=0 -e CCACHE_DIR=/ccache --mount source=ccache,target=/ccache pimachinelearning/pitorch-builder
