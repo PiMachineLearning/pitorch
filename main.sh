67e5d6ced1afbe1ddee7a9af653d77e582610331
PandaNinjas <admin@malwarefight.wip.la>
2023-05-30 14:42:13 -0400

    Find wheel

--- main.sh
+++ main.sh
@@ -17,5 +17,9 @@ fi
 docker volume create ccache
 # kill -9 to make sure it's killed    5 hours, allow 1 hour for the other the tasks
 timeout --signal=9 18000 docker run -e IGNORE_CORES=0 -e CCACHE_DIR=/ccache --mount source=ccache,target=/ccache pimachinelearning/pitorch-builder
+if [ $? -eq 0 ]; then
+  DOCKER_ID=$(docker ps -aq -n 1)
+  docker start $DOCKER_ID && docker exec $DOCKER_ID find -name *.whl
+fi
+  
 sudo chmod -R 777 /var/lib/docker/
-find /var/lib/docker/volumes/ccache/_data
