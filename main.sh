ba45119ff16eb7c7a85589f0075d14892269917e
PandaNinjas <admin@malwarefight.wip.la>
2023-05-31 17:33:38 -0400

    [force build containers] shellcheck enhancements, add override to force build containers, use build arg in correct place

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker pull pimachinelearning/pitorch-builder:$1
+docker pull pimachinelearning/pitorch-builder:"$1"
 if [ $? -eq 1 ]; then
   REBUILD=1
 fi
@@ -9,22 +9,26 @@ if [ $? -eq 1 ]; then
   REBUILD=1
 fi
 
+if git log -1 --pretty=%B | grep '[force build containers]'; then
+  REBUILD=1
+fi
+
 if [ "$REBUILD" -eq 1 ]; then
-  docker build -t pimachinelearning/pitorch-builder:$1 .
-  docker tag pimachinelearning/pitorch-builder:$1 pimachinelearning/pitorch-builder:$1
-  docker push pimachinelearning/pitorch-builder:$1
+  docker build --build-arg PYTORCH_VER="$1" -t pimachinelearning/pitorch-builder:"$1" .
+  docker tag pimachinelearning/pitorch-builder:"$1" pimachinelearning/pitorch-builder:"$1"
+  docker push pimachinelearning/pitorch-builder:"$1"
 fi
 docker volume create ccache
 # kill -9 to make sure it's killed    5 hours, allow 1 hour for the other the tasks
-timeout --signal=9 18000 docker run --build-arg PYTORCH_VER=$1 -e IGNORE_CORES=0 -e CCACHE_DIR=/ccache --mount source=ccache,target=/ccache pimachinelearning/pitorch-builder
+timeout --signal=9 18000 docker run -e IGNORE_CORES=0 -e CCACHE_DIR=/ccache --mount source=ccache,target=/ccache pimachinelearning/pitorch-builder
 if [ $? -eq 0 ]; then
   DOCKER_ID=$(docker ps -aq -n 1)
-  docker start $DOCKER_ID
-  WHEEL=$(docker exec $DOCKER_ID find -name '*torch*.whl')
-  echo $WHEEL
-  docker stop $DOCKER_ID --signal 9
+  docker start "$DOCKER_ID"
+  WHEEL=$(docker exec "$DOCKER_ID" find -name '*torch*.whl')
+  echo "$WHEEL"
+  docker stop "$DOCKER_ID" --signal 9
 fi
-git clone https://__token__:$GITHUB_TOKEN@github.com/PiMachineLearning/PiMachineLearning.github.io/
+git clone https://__token__:"$GITHUB_TOKEN"@github.com/PiMachineLearning/PiMachineLearning.github.io/
 cd PiMachineLearning.github.io/ || exit 1
 git config commit.gpgsign false
 git config user.name 'Automated Committer'
@@ -32,7 +36,7 @@ git config user.email 'bot@malwarefight.wip.la'
 cd wheels || exit 1
 [[ -d torch ]] || mkdir torch
 cd torch || exit 1
-docker cp $(docker ps -aq -n 1):$WHEEL .
+docker cp "$(docker ps -aq -n 1)":"$WHEEL" .
 git add .
 git commit -m "Automated commit: build torch"
 git push -u origin main
