fb024fd54cdfa58c53bf9466c09dea148b716db9
PandaNinjas <admin@malwarefight.wip.la>
2023-06-04 00:35:08 -0400

    Finally fix wheel versioning

--- main.sh
+++ main.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 docker pull pimachinelearning/pitorch-builder:"$1"
+
+REBUILD=0
 if [ $? -eq 1 ]; then
   REBUILD=1
 fi
@@ -14,13 +16,14 @@ if git log -1 --pretty=%B | grep '\[force build containers\]'; then
 fi
 
 if [ "$REBUILD" -eq 1 ]; then
+  echo "Rebuilding..."
   docker build --build-arg PYTORCH_VER="$1" -t pimachinelearning/pitorch-builder:"$1" . || exit 1
   docker tag pimachinelearning/pitorch-builder:"$1" pimachinelearning/pitorch-builder:"$1"
   docker push pimachinelearning/pitorch-builder:"$1"
 fi
 docker volume create ccache
 # kill -9 to make sure it's killed    5 hours, allow 1 hour for the other the tasks
-timeout --signal=9 18000 docker run -e PYTORCH_VER=$1 -e IGNORE_CORES=0 -e CCACHE_DIR=/ccache --mount source=ccache,target=/ccache pimachinelearning/pitorch-builder
+timeout --signal=9 18000 docker run -e PYTORCH_BUILD_NUMBER=0 -e PYTORCH_BUILD_VERSION="$1" -e IGNORE_CORES=0 -e CCACHE_DIR=/ccache --mount source=ccache,target=/ccache pimachinelearning/pitorch-builder
 DOCKER_ID=$(docker ps -aq -n 1)
 docker start "$DOCKER_ID"
 WHEEL=$(docker exec "$DOCKER_ID" find -name '*torch*.whl')
